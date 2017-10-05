a8ac52642862d86bb82f7c3fa4c3a1cc40d9bc90
Tej Pochiraju <tej.pochiraju@gmail.com>
2017-10-05 13:56:18 +0530

    minor edits

--- gulpfile.js
+++ gulpfile.js
@@ -15,40 +15,42 @@ var customProperties = require('postcss-custom-properties');
 var easyimport = require('postcss-easy-import');
 
 var swallowError = function swallowError(error) {
-    gutil.log(error.toString());
-    gutil.beep();
-    this.emit('end');
+  gutil.log(error.toString());
+  gutil.beep();
+  this.emit('end');
 };
 
-var nodemonServerInit = function () {
-    livereload.listen(1234);
+var nodemonServerInit = function() {
+  livereload.listen(1234);
 };
 
-gulp.task('build', ['css'], function (/* cb */) {
-    return nodemonServerInit();
+gulp.task('build', ['css'], function( /* cb */ ) {
+  return nodemonServerInit();
 });
 
-gulp.task('css', function () {
-    var processors = [
-        easyimport,
-        customProperties,
-        colorFunction(),
-        autoprefixer({browsers: ['last 2 versions']}),
-        cssnano()
-    ];
-    gulp.src('assets/css/*.css')
-        .on('error', swallowError)
-        .pipe(sourcemaps.init())
-        .pipe(postcss(processors))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('assets/built/'))
-        .pipe(livereload());
+gulp.task('css', function() {
+  var processors = [
+    easyimport,
+    customProperties,
+    colorFunction(),
+    autoprefixer({
+      browsers: ['last 2 versions']
+    }),
+    cssnano()
+  ];
+  gulp.src('assets/css/*.css')
+    .on('error', swallowError)
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/built/'))
+    .pipe(livereload());
 });
 
-gulp.task('watch', function () {
-    gulp.watch('assets/css/**', ['css']);
+gulp.task('watch', function() {
+  gulp.watch('assets/css/**', ['css']);
 });
 
-gulp.task('default', ['build'], function () {
-    gulp.start('watch');
+gulp.task('default', ['build'], function() {
+  gulp.start('watch');
 });
